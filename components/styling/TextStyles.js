b16f22f2b8ccd48673d4399904cea1c4021da60f
Ashley Oldershaw <ash.oldershaw@gmail.com>
2022-03-04 22:18:02 +0000

    text resizing

--- components/styling/TextStyles.js
+++ components/styling/TextStyles.js
@@ -1,13 +1,15 @@
 import styled from "styled-components";
 
-const fontFamily = `'Caveat', serif`
+const fontFamily = `'Caveat', serif`;
 
 export const PageTitle = styled.h1`
-font-family: ${fontFamily};
-font-size: 64px;
-font-weight: 10;
+  font-family: ${fontFamily};
+  font-size: 64px;
+  font-weight: 10;
 `;
 
 export const PageSubtitle = styled.h2`
-font-family: ${fontFamily};
-`
+  font-family: ${fontFamily};
+  font-size: 32px;
+  font-weight: 10;
+`;
