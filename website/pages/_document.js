88b39a71f28dbdb0f01d37e4171fba109a56f93f
Ash Oldershaw <ash.oldershaw@gmail.com>
2022-03-08 22:01:31 +0000

    fixing font substitution

--- website/pages/_document.js
+++ website/pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Caveat&family=Smooch+Sans&display=optional"
+            href="https://fonts.googleapis.com/css2?family=Caveat&family=Smooch+Sans&display=swap"
             rel="stylesheet"
           />
         </Head>
