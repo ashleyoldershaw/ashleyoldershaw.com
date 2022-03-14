8cdf0f5fe4b754fc8d74dc313c063f63d0c5edd5
Ash Oldershaw <ash.oldershaw@gmail.com>
2022-03-14 19:17:52 +0000

    blocking page load before font is ready

--- website/pages/_document.js
+++ website/pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Caveat&family=Montserrat:wght@300&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Caveat&family=Montserrat:wght@300&display=block"
             rel="stylesheet"
           />
         </Head>
