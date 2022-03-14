00292df69e2c549a59bb221f1f531d2148c31fdc
Ash Oldershaw <ash.oldershaw@gmail.com>
2022-03-14 19:10:05 +0000

    use next link

--- website/components/utility/SmartLink.js
+++ website/components/utility/SmartLink.js
@@ -1,7 +1,26 @@
+import Link from "next/link";
+import { Fragment } from "react";
+import styled from "styled-components";
+
+const LinkStyle = styled.div`
+  :hover {
+    cursor: pointer;
+  }
+`;
+
 export const SmartLink = ({ href, children }) => {
+  if (href.charAt(0) === "/") {
+    return (
+      <LinkStyle>
+        <Link href={href}>{children}</Link>
+      </LinkStyle>
+    );
+  }
   return (
-    <a href={href} {...(href.charAt(0) === "/" ? {} : { target: "_blank" })}>
-      {children}
-    </a>
+    <LinkStyle>
+      <a href={href} target="_blank">
+        {children}
+      </a>
+    </LinkStyle>
   );
 };
