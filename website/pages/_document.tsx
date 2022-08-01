d03993f4b70d62d71cec8279f91bbc6bda9573d8
Ash Oldershaw <ash.oldershaw@gmail.com>
2022-08-01 22:20:04 +0100

    adding cloudflare analytics

--- website/pages/_document.tsx
+++ website/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
   render() {
@@ -13,6 +14,12 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <Script
+            strategy="afterInteractive"
+            defer={true}
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+            data-cf-beacon={`{"token": "${process.env.NEXT_PUBLIC_CLOUDFLARE_ANALYTICS_TOKEN}"}`}
+          ></Script>
         </body>
       </Html>
     );
